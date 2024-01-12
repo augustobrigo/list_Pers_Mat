620801ddcd949d07a21714e29d705f0765e2ac55
augustobrigo <profeaugustobriga@gmail.com>
2024-01-12 11:50:32 +0100

    LISTADO_Empleados_dam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,FormsModule, MatTableModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
